12beb1a97e200871a91ae96961b9f31bdd2ea768
Jeremy M. G. Leung <jml230@pitt.edu>
2024-06-14 15:38:34 -0400

    end up in a nice place

--- run_bash.sh
+++ run_bash.sh
@@ -7,8 +7,10 @@ ln -s $TMPDIR
 python -m venv westpa-workshop2024
 source ~/westpa-workshop2024/bin/activate
 
-# Clone WESTPA workshop github repo
+# Clone WESTPA workshop github repo and install everything into virtualenv
 cd $TMPDIR
 git clone https://github.com/westpa/westpa-workshop2024-tutorials
-cd westpa-workshop2024-tutorials/ntl9-synd-folding
+cd westpa-workshop2024-tutorials/ntl9_sample_files
 python -m pip install -U -r requirements.txt
+cd ../
+echo "Current working directory is $PWD"
