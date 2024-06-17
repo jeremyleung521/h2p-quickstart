0c02208a43d954eaba28ea7ccdae523cf53baaa4
Jeremy M. G. Leung <jml230@pitt.edu>
2024-06-17 12:57:11 -0400

    update activate_env.sh

--- activate_env.sh
+++ activate_env.sh
@@ -3,4 +3,5 @@
 source ~/westpa-workshop2024/bin/activate
 export OMP_NUM_THREADS=1
 export RAY_worker_register_timeout_seconds=60
+git clone https://github.com/westpa/westpa-workshop2024-tutorials $TMPDIR
 echo "Current working directory is $PWD"
